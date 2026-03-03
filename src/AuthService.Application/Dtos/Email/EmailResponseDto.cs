aa46358bff25c0b9e6d33a4cf994aefaf66aaca6
Marioescobar64 <mescobar-2022471@kinal.edu.gt>
2026-03-03 17:25:57 -0600

    fix/ restauracion de las rutas

--- src/AuthService.Application/Dtos/Email/EmailResponseDto.cs
+++
@@ -1,8 +0,0 @@
-namespace AuthService.Application.DTOs.Email;
- 
-public class EmailResponseDto
-{
-    public bool Succes { get; set; }
- 
-    public string Message { get; set; } = string.Empty;
-}
