1754294fbec536813d016b9eee5c715193b9f328
DAMB1808 <dmauricio-2022464@kinal.edu.gt>
2026-04-14 10:04:20 -0600

    fix/ Se realizaron modificaciones

--- src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
+++ src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26267a9ee281c68e7d2ded0fbf340270f0155cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916a5b5c4ed40111375df65dea685d497cbb9bc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
+++ src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52f6356a6a109e31c8f4466c29f4ae90811edff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916a5b5c4ed40111375df65dea685d497cbb9bc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
